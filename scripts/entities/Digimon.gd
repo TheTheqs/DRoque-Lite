5beeeede2554cdd9f95e6528b542673c63c9fb4b
Matheqs <matheqs@outlook.com>
2024-10-03 08:00:15 -0300

    Initial Interface Tests

---
+++ scripts/entities/Digimon.gd
@@ -0,0 +1,22 @@
+extends Node
+
+class_name Digimon
+
+#Database info
+var DigimonId: int
+var DigimonName: String
+var DigimonDescription: String
+
+#Scene Elements
+@export var DigimonSprite: Sprite2D
+@export var DigimonAnimator: AnimationPlayer
+
+#Status Control
+var ItsDisable: bool = false
+var ItsBlind: bool = false
+
+func _ready():
+	if(not ItsDisable):
+		DigimonAnimator.play("Idle")
+	if(self.position.x < 180):
+		DigimonSprite.flip_h = true
